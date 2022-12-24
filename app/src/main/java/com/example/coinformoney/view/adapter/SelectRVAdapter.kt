4f6aeee055e60f96abe37061c31e60f317ad48ea
DK <spol@naver.com>
2022-12-24 18:27:16 +0900

    코인 선택화면 Adapter 값 매핑(RecyclerView)

--- app/src/main/java/com/example/coinformoney/view/adapter/SelectRVAdapter.kt
+++ app/src/main/java/com/example/coinformoney/view/adapter/SelectRVAdapter.kt
@@ -1,26 +1,42 @@
 package com.example.coinformoney.view.adapter
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.coinformoney.R
 import com.example.coinformoney.datamodel.CurrentPriceResult
 
 class SelectRVAdapter(val context: Context, val coinPriceList: List<CurrentPriceResult>) :
-    RecyclerView.Adapter<SelectRVAdapter.ViewHolder>(){
+    RecyclerView.Adapter<SelectRVAdapter.ViewHolder>() {
 
-        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
-
-        }
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val coinName: TextView = view.findViewById(R.id.coinName)
+        val coinPriceUpDown: TextView = view.findViewById(R.id.coinPriceUpDown)
+        val likeImage : ImageView = view.findViewById(R.id.likeBtn)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.intro_coin_item,parent,false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.intro_coin_item, parent, false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        holder.coinName.text = coinPriceList[position].coinName
+        val fluctate_24H = coinPriceList[position].coinInfo.fluctate_24H
+        if (fluctate_24H.contains("-")){
+            holder.coinPriceUpDown.text = "하락입니다."
+            holder.coinPriceUpDown.setTextColor(Color.parseColor("#114fed"))
+        }else {
+            holder.coinPriceUpDown.text = "상승입니다."
+            holder.coinPriceUpDown.setTextColor(Color.parseColor("#ed2e11"))
+        }
 
     }
 
