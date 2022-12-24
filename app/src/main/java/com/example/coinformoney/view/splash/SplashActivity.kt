5b39a60e7a81a99b458408f8d3de6ea3b2ef9bdf
DK <spol@naver.com>
2022-12-25 03:26:29 +0900

    처음 접속하면 visible 처음이 아니면 Invisible로 세팅

--- app/src/main/java/com/example/coinformoney/view/splash/SplashActivity.kt
+++ app/src/main/java/com/example/coinformoney/view/splash/SplashActivity.kt
@@ -1,16 +1,41 @@
 package com.example.coinformoney.view.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.Observer
+import com.example.coinformoney.MainActivity
 import com.example.coinformoney.R
+import com.example.coinformoney.databinding.ActivitySplashBinding
+
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivitySplashBinding
+    private val viewModel: SplashViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         installSplashScreen()
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel.checkFirstFlag()
+        viewModel.first.observe(this, Observer {
+            if(it){
+                // 처음이 아닌 유저
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }else{
+                // 처음 접속하는 유저
+                binding.fragmentContainerView.visibility = View.VISIBLE
+            }
+        })
 
     }
 }
--- app/src/main/java/com/example/coinformoney/view/splash/SplashViewModel.kt
+++ app/src/main/java/com/example/coinformoney/view/splash/SplashViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.coinformoney.view.splash
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.coinformoney.dataStore.MyDataStore
@@ -7,8 +9,14 @@ import kotlinx.coroutines.launch
 
 class SplashViewModel : ViewModel() {
 
+    private val _first = MutableLiveData<Boolean>()
+    val first: LiveData<Boolean>
+        get() = _first
+
     fun checkFirstFlag() = viewModelScope.launch {
         val getData = MyDataStore().getFirstData()
+
+        _first.value = getData
     }
 
 
